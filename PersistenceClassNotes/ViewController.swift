5fcde8623bc0bd48a29ce0d7521a47b7c78101a3
Daniel Vega <daniel.vega23@d155.org>
2021-10-22 08:16:18 -0500

    dfba

--- PersistenceClassNotes/ViewController.swift
+++ PersistenceClassNotes/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public struct Contact: Codable {
+    var name: String
+    var age: Int
+}
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+  
+    @IBOutlet weak var nameOutlet: UITextField!
+    @IBOutlet weak var ageOutlet: UITextField!
+    
+    //let defaults = UserDefaults.standard
+
+    @IBOutlet weak var tableViewOutlet: UITableView!
+    
+    var contacts = [Contact]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableViewOutlet.delegate = self
+        tableViewOutlet.dataSource = self
+        
+        contacts.append(Contact(name: "Brian", age: 27))
+        contacts.append(Contact(name: "Tracy", age: 47))
+        
+        if let items = UserDefaults.standard.data(forKey: "theContacts2") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([Contact].self, from: items){
+                contacts = decoded
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = contacts[indexPath.row].name
+        cell.detailTextLabel?.text = String(contacts[indexPath.row].age)
+        return cell
+    }
 
+    
+    @IBAction func addAction(_ sender: UIButton) {
+     
+        let encoder = JSONEncoder()
+        
+        if let encoded = try? encoder.encode(contacts) {
+            
+            UserDefaults.standard.set(contacts, forKey: "theContacts2")}
+    }
+    
+    @IBAction func frAddAction(_ sender: UIButton) {
+        contacts.append(Contact(name: nameOutlet.text!, age: Int(ageOutlet.text!)!))
+        tableViewOutlet.reloadData()
+    }
 }
 
